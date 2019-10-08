3ba1447b8f94bc27708ee5ad88af5ef7df43de00
Huusain_AlHaddad <h.ali.alhaddad@hotmail.com>
2019-10-08 10:34:34 +0300

    shopping cart with badge

--- Components/CartButton/index.js
+++ Components/CartButton/index.js
@@ -1,17 +1,43 @@
 import React, { Component } from "react";
-import { Icon } from "native-base";
+import { Icon, View, Text, Right } from "native-base";
 import { withNavigation } from "react-navigation";
+import { connect } from "react-redux";
 
 class CartButton extends Component {
   render() {
+    let badge = 0;
+    this.props.items.forEach(item => (badge += item.quantity));
     return (
-      <Icon
-        onPress={() => this.props.navigation.navigate("CoffeeCart")}
-        name="shoppingcart"
-        type="AntDesign"
-      />
+      <View>
+        <Text
+          style={{
+            backgroundColor: "red",
+            color: "white",
+            textAlign: "center",
+            fontSize: 10,
+            height: 15,
+            alignSelf: "center",
+            paddingRight: 4,
+            paddingLeft: 5,
+            borderRadius: 50
+          }}
+        >
+          {badge}
+        </Text>
+        <Right>
+          <Icon
+            onPress={() => this.props.navigation.navigate("CoffeeCart")}
+            name="cart"
+            style={{ right: 10, bottom: 10, color: "white" }}
+          />
+        </Right>
+      </View>
     );
   }
 }
 
-export default withNavigation(CartButton);
+const mapStateToProps = state => ({
+  items: state.cartReducer.items
+});
+
+export default withNavigation(connect(mapStateToProps)(CartButton));
